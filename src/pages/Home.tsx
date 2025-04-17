0af3fa632aa79cdc2e50d16e0bd02b1e1d975506
colehallberg <hallbergc@g.cofc.edu>
2025-04-17 00:25:04 -0400

    Update styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import './style.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
-    return <h1>Welcome to Cal-culator 🍽️</h1>;
+  return(
+  <div className="custom-container">
+  <h1 className="custom-heading">Welcome to Cal-culator!</h1>
+      <p className="custom-text">
+        Already have an account?{" "}
+        <Link to="/login" className="custom-link">
+        Login
+        </Link>
+        <br />
+        Don't have an account?{" "}
+        <Link to="/register" className="custom-link">
+        Register
+        </Link>
+      </p> 
+      </div>
+  );
   }
   
   export default Home;
