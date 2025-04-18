a2f85079e8a90f36961a82c107dc6d31de55cc80
colehallberg <hallbergc@g.cofc.edu>
2025-04-17 22:40:08 -0400

    Updated styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,32 @@ import './style.css';
 import { Link } from 'react-router-dom';
 
 function Home() {
-  return(
-  <div className="custom-container">
-  <h1 className="custom-heading">Welcome to Cal-culator!</h1>
-      <p className="custom-text">
-        Already have an account?{" "}
-        <Link to="/login" className="custom-link">
-        Login
-        </Link>
-        <br />
-        Don't have an account?{" "}
-        <Link to="/register" className="custom-link">
-        Register
-        </Link>
-      </p> 
+  return (
+    <div className="landing-container">
+      <div className="landing-header">
+        <h1 className="main-heading">Cal-culator</h1>
+        <p className="sub-heading">
+          Easy to use application to track your daily calorie intake
+        </p>
+        
+        <br></br>
+        <div className="auth-options">
+          <div className="auth-option">
+            <span>Already have an account? </span>
+            <Link to="/login" className="custom-link auth-link">
+              Login
+            </Link>
+          </div>
+          <div className="auth-option">
+            <span>Don't have an account? </span>
+            <Link to="/register" className="custom-link auth-link">
+              Register
+            </Link>
+          </div>
+        </div>
       </div>
+    </div>
   );
-  }
-  
-  export default Home;
-  
+}
+
+export default Home;
